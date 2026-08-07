90319acf29c10e7f05ce8882a30a8a29c3ba41b9
Ariel Quintana <aquintana@fanki.co>
2026-08-07 16:59:37 -0300

    add edit user

--- BaseProject/BaseProject.Shared/Entities/City.cs
+++ BaseProject/BaseProject.Shared/Entities/City.cs
@@ -1,7 +1,5 @@
 ﻿using BaseProject.Shared.Interfaces;
-using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using System.ComponentModel.DataAnnotations;
-using System.Text.Json.Serialization;
 
 namespace BaseProject.Shared.Entities;
 
@@ -16,8 +14,6 @@ public class City : IEntityWithName
 
     public int StateId { get; set; }
 
-    [JsonIgnore]
-    [ValidateNever]
     public State? State { get; set; } = null!;
 
     public ICollection<User>? Users { get; set; }
--- BaseProject/BaseProject.Shared/Entities/State.cs
+++ BaseProject/BaseProject.Shared/Entities/State.cs
@@ -1,7 +1,5 @@
 ﻿using BaseProject.Shared.Interfaces;
-using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using System.ComponentModel.DataAnnotations;
-using System.Text.Json.Serialization;
 
 namespace BaseProject.Shared.Entities;
 
@@ -16,8 +14,6 @@ public class State : IEntityWithName
 
     public int CountryId { get; set; }
 
-    [JsonIgnore]
-    [ValidateNever]
     public Country Country { get; set; } = null!;
 
     public ICollection<City>? Cities { get; set; }
